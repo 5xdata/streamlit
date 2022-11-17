a6e7719462253e66a99d532fc5dbfacc8f2c112a
naveen5x <101312486+naveen5x@users.noreply.github.com>
2022-11-17 16:04:39 +0800

    Update us_accounts_sf.py

--- us_accounts_sf.py
+++ us_accounts_sf.py
@@ -23,7 +23,7 @@ def run_query(query):
         return cur.fetchall()
 
 rows = run_query("SELECT billing_address, billing_latitude, billing_longitude, sum(acv) as active_acv from wevideo_analytics.salesforce.active_acv where billing_latitude is not null and billing_longitude is not null and date = last_day(current_date) group by 1,2,3 order by 4 desc ;")
-
+st.write(rows)
 # Print results.
 st.title('Raw Data')
 def load_data1():
