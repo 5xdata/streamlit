2292628df45678da5a55eb0737d128d5d9bd362d
naveen5x <101312486+naveen5x@users.noreply.github.com>
2022-11-17 13:41:27 +0800

    Create us_accounts_sf.py

---
+++ us_accounts_sf.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import streamlit as st
+import pydeck as pdk
+import numpy as np
+import snowflake.connector
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return snowflake.connector.connect(
+        **st.secrets["snowflake"], client_session_keep_alive=True
+    )
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT billing_city, billing_state, billing_latitude, billing_longitude, active_acv from wevideo_analytics.salesforce.active_acv where billing_latitude is not null and billing_longitude is not null and date = last_day(current_date) ;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
+
+st.title('US Accounts')
+
+acv_data = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTUgpBzoRCnUYYA85IKII2TwgLHsPAlKoLvZ5LByqP5hSCPNzkMZYH2wVdKezUPXdmdpdU3FKF1hAtC/pub?gid=0&single=true&output=csv'
+
+@st.cache
+def load_data():
+    data = pd.read_csv(acv_data)
+    data.rename(columns = {'billing_latitude':'lat', 'billing_longitude':'lon'}, inplace = True)
+    return data
+
+data_load_state = st.text('Loading data...')
+data = load_data()
+data_load_state.text("Done by Naveen!!")
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+    
+st.subheader('Map of all US Accounts of WeVideo with active ACV')
+
+viewState = pdk.ViewState(
+    longitude=-112.8591427000004, latitude=36.365390013524475, zoom=3.50, bearing=0, pitch=0
+)
+mapStyle = 'mapbox://styles/wevideo/claavllul000m14qw11d227ne'
+
+acv_to_filter = st.slider('active_acv', 0, 20000, 1000)
+filtered_data = data[data['active_acv'] == acv_to_filter]
+
+scatterLayer = pdk.Layer(
+    'ScatterplotLayer',      
+    filtered_data,
+    get_position='[lon, lat]',
+    pickable=True,
+    opacity=0.2,
+    stroked=True,
+    filled=True,
+    radius_scale=6,
+    radius_min_pixels=1,
+    radius_max_pixels=10,
+    line_width_min_pixels=1,
+    get_radius='[active_acv]',
+    get_fill_color=[255, 100, 80, 140],
+    get_line_color=[0, 0, 0],
+    )
+
+hexLayer = pdk.Layer(
+    'HexagonLayer',
+    data=filtered_data,
+    get_position='[lon, lat]',
+    radius='[active_acv]',
+    elevation_scale=4,
+    elevation_range=[0, 10000],
+    pickable=True,
+    extruded=True,
+    )
+        
+toolTip = {
+    "html": "<b>Account - My Wevideo Account</b><br><b>Billing Address - {billing_address}</b><br><b>Active ACV - {active_acv}</b>",
+    "style": {"background": "grey", "color": "white", "font-family": '"Helvetica Neue", Arial', "z-index": "10000", "max-width": "25%"},
+}
+
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=viewState,
+    layers=[hexLayer, scatterLayer],
+    tooltip= toolTip
+    ))
