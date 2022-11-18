cd0d72fe7afb6b528962ec831676d9f8a2c5c531
naveen5x <101312486+naveen5x@users.noreply.github.com>
2022-11-18 23:06:18 +0800

    Update us_accounts_sf.py

--- us_accounts_sf.py
+++ us_accounts_sf.py
@@ -77,18 +77,18 @@ if test.empty:
 else:
     filtered = test
 filtered_data = pd.DataFrame(filtered, columns=['account_name','city','state','zip','lat','lon','active_acv'])  
-'''
+
 city = st.multiselect(
     'Choose the cities that you want to filter'
     ,sorted(set(filtered_data['city'].str.lower())))
 
-test = filtered_data = filtered_data.loc[filtered_data['city'].str.lower().isin(city)]  
+test = filtered_data.loc[filtered_data['city'].str.lower().isin(city)]  
 if test.empty:
     filtered = filtered_data
 else:
     filtered = test
 filtered_data = pd.DataFrame(filtered, columns=['account_name','city','state','zip','lat','lon','active_acv'])  
-'''
+
 scatterLayer = pdk.Layer(
     'ScatterplotLayer',      
     filtered_data,
