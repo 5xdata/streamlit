ea752c8e872d9f3a9bebc961b75fefc52e327304
naveen5x <101312486+naveen5x@users.noreply.github.com>
2022-11-18 14:32:55 +0800

    Update us_accounts_sf.py

--- us_accounts_sf.py
+++ us_accounts_sf.py
@@ -53,11 +53,11 @@ viewState = pdk.ViewState(
 
 city = st.multiselect(
     'Choose the cities that you want to filter'
-    ,sorted(set(data['city'] #.str.lower())))
+    ,sorted(set(data['city']))) #.str.lower())))
 
 state = st.multiselect(
     'Choose the states that you want to filter'
-    ,sorted(set(data['state'] #.str.lower())))
+    ,sorted(set(data['state']))) #.str.lower())))
 
 filtered_data = data[data[['city','state']].isin([city,state]).any(axis=1)]
     #dataframe1=dataframe[dataframe[['Subject', 'Semester']].isin(["SDA", '7th']).any(axis=1)]
