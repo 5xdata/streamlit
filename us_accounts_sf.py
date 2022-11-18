5e883f201b8488e25b7cc5cf3f3234f59b2a451b
naveen5x <101312486+naveen5x@users.noreply.github.com>
2022-11-18 10:57:19 +0800

    Update us_accounts_sf.py

--- us_accounts_sf.py
+++ us_accounts_sf.py
@@ -22,14 +22,14 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT concat(split_part(billing_address,',',3)||','||split_part(billing_address,',',4)||','||split_part(billing_address,',',-1)), billing_latitude, billing_longitude, sum(acv) as active_acv from wevideo_analytics.salesforce.active_acv where billing_latitude is not null and billing_longitude is not null and date = last_day(current_date) group by 1,2,3 order by 4 desc ;")
+rows = run_query("SELECT sub_str(account_name,1,3) as account_name, split_part(billing_address,',',3) as city, split_part(billing_address,',',4) as state, split_part(billing_address,',',-1) as zip, billing_latitude, billing_longitude, sum(acv) as active_acv from wevideo_analytics.salesforce.active_acv where billing_latitude is not null and billing_longitude is not null and date = last_day(current_date) group by 1,2,3 order by 4 desc ;")
 
 st.title('US Accounts')
 # Print results.
 
 @st.cache
 def load_data():
-    return pd.DataFrame(rows, columns=['city_state_zip','lat','lon','active_acv'])
+    return pd.DataFrame(rows, columns=['account_name','city','state','zip','lat','lon','active_acv'])
 
 data_load_state = st.text('Loading data...')
 data = load_data()
@@ -68,7 +68,7 @@ scatterLayer = pdk.Layer(
     )
 
 toolTip = {
-    "html": "<b>City State Zip - {city_state_zip}</b><br><b>Active ACV - {active_acv}</b>",
+    "html": "<b>Account Name (3 charac) - {account_name}</b><br><b>City - {city}</b><br><b>State - {state}</b><br><b>Zip - {zip}</b><br><b>Active ACV - {active_acv}</b>",
     "style": {"background": "grey", "color": "white", "font-family": '"Helvetica Neue", Arial', "z-index": "10000", "max-width": "30%"},
 }
 
