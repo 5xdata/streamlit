e808ea9aa466afe2957e2ed36a5b4218ac0dfa0c
naveen5x <101312486+naveen5x@users.noreply.github.com>
2022-11-19 00:31:41 +0800

    Update us_accounts_sf.py

--- us_accounts_sf.py
+++ us_accounts_sf.py
@@ -34,75 +34,71 @@ data_load_state = st.text('Loading data...')
 data = load_data()
 data_load_state.text("Data refreshed and loaded !!")
 
-
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.checkbox("Full Screen", value=False, key="use_container_width")
-    st.dataframe(data, use_container_width=st.session_state.use_container_width)
-    
 st.subheader('Map of all US Accounts of WeVideo with active ACV')
 
-viewState = pdk.ViewState(
-    longitude=-112.8591427000004, latitude=36.365390013524475, zoom=3.50, bearing=0, pitch=0
-)
+if st.checkbox('Show Filters'):
+    st.subheader('Filters for Active ACV, State and City')
+    st.dataframe(data, use_container_width=True)
+
+    acv_range = st.radio(
+        'Choose the acv range'
+        ,('< $500', '$500 - $3k', '$3k - $10k', '> $10k','All Accounts'))
 
 
-acv_range = st.radio(
-    'Choose the acv range'
-    ,('< $500', '$500 - $3k', '$3k - $10k', '> $10k','All Accounts'))
+    if acv_range == '< $500':
+        filtered_data = data.loc[(data['active_acv'] > 0) & (data['active_acv'] < 500)]
+        acv_filter = '< $500'
+    elif acv_range =='$500 - $3k':
+        filtered_data = data.loc[(data['active_acv'] >=500) & (data['active_acv'] < 3000)]
+        acv_filter = '$500 - $3k'
+    elif acv_range == '$3k - $10k':
+        filtered_data = data.loc[(data['active_acv'] >= 3000) & (data['active_acv'] < 10000)]
+        acv_filter = '$3k - $10k'
+    elif acv_range == '> $10k':
+        filtered_data = data.loc[(data['active_acv'] >= 10000)]
+        acv_filter = '$3k - $10k'
+    else:
+        filtered_data = data
+        acv_filter = 'of any value'
+
+    filtered_data = pd.DataFrame(filtered_data, columns=['account_name','city','state','zip','lat','lon','active_acv'])   
 
 
-if acv_range == '< $500':
-    filtered_data = data.loc[(data['active_acv'] > 0) & (data['active_acv'] < 500)]
-    acv_filter = '< $500'
-elif acv_range =='$500 - $3k':
-    filtered_data = data.loc[(data['active_acv'] >=500) & (data['active_acv'] < 3000)]
-    acv_filter = '$500 - $3k'
-elif acv_range == '$3k - $10k':
-    filtered_data = data.loc[(data['active_acv'] >= 3000) & (data['active_acv'] < 10000)]
-    acv_filter = '$3k - $10k'
-elif acv_range == '> $10k':
-    filtered_data = data.loc[(data['active_acv'] >= 10000)]
-    acv_filter = '$3k - $10k'
+    state = st.multiselect(
+        'Choose the states that you want to filter'
+        ,sorted(set(filtered_data['state'].str.lower())))
+
+    dummy = filtered_data.loc[filtered_data['state'].str.lower().isin(state)]  
+    if dummy.empty:
+        filtered = filtered_data
+        state_filter = 'any state'
+    else:
+        filtered = dummy
+        converted_list = [str(element) for element in state]
+        state_filter = ",".join(converted_list)
+
+    filtered_data = pd.DataFrame(filtered, columns=['account_name','city','state','zip','lat','lon','active_acv'])  
+
+    city = st.multiselect(
+        'Choose the cities that you want to filter'
+        ,sorted(set(filtered_data['city'].str.lower())))
+
+    dummy = filtered_data.loc[filtered_data['city'].str.lower().isin(city)]  
+    if dummy.empty:
+        filtered = filtered_data
+        city_filter = 'any city'
+    else:
+        filtered = dummy
+        converted_list = [str(element) for element in city]
+        city_filter = ",".join(converted_list)
+
+    filtered_data = pd.DataFrame(filtered, columns=['account_name','city','state','zip','lat','lon','active_acv'])  
+
+    st.write("Total WeVideo Accounts with Active ACV",acv_filter,", in State(s)",state_filter,"and in City(s)",city_filter,"is (are)",len(filtered_data)) 
+    st.dataframe(filtered_data, use_container_width = True)
 else:
     filtered_data = data
-    acv_filter = 'of any value'
-
-filtered_data = pd.DataFrame(filtered_data, columns=['account_name','city','state','zip','lat','lon','active_acv'])   
-
-
-state = st.multiselect(
-    'Choose the states that you want to filter'
-    ,sorted(set(filtered_data['state'].str.lower())))
-
-dummy = filtered_data.loc[filtered_data['state'].str.lower().isin(state)]  
-if dummy.empty:
-    filtered = filtered_data
-    state_filter = 'any state'
-else:
-    filtered = dummy
-    converted_list = [str(element) for element in state]
-    state_filter = ",".join(converted_list)
-
-filtered_data = pd.DataFrame(filtered, columns=['account_name','city','state','zip','lat','lon','active_acv'])  
-
-city = st.multiselect(
-    'Choose the cities that you want to filter'
-    ,sorted(set(filtered_data['city'].str.lower())))
-
-dummy = filtered_data.loc[filtered_data['city'].str.lower().isin(city)]  
-if dummy.empty:
-    filtered = filtered_data
-    city_filter = 'any city'
-else:
-    filtered = dummy
-    converted_list = [str(element) for element in city]
-    city_filter = ",".join(converted_list)
-
-filtered_data = pd.DataFrame(filtered, columns=['account_name','city','state','zip','lat','lon','active_acv'])  
-
-st.write("Total WeVideo Accounts with Active ACV",acv_filter,", in State(s)",state_filter,"and in City(s)",city_filter,"is (are)",len(filtered_data)) 
-st.dataframe(filtered_data, use_container_width = True)
+    
 scatterLayer = pdk.Layer(
     'ScatterplotLayer',      
     filtered_data,
@@ -119,6 +115,11 @@ scatterLayer = pdk.Layer(
     get_fill_color=[255, 100, 80, 140],
     get_line_color=[0, 0, 0],
     )
+
+
+viewState = pdk.ViewState(
+    longitude=-112.8591427000004, latitude=36.365390013524475, zoom=3.50, bearing=0, pitch=0
+)
 
 toolTip = {
     "html": "<b>Account Name (3 charac) - {account_name}</b><br><b>City - {city}</b><br><b>State - {state}</b><br><b>Zip - {zip}</b><br><b>Active ACV - {active_acv}</b>",
