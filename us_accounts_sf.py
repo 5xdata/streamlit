782a2c32b6c8cd1bec4f00b5bff180e27ffadc01
naveen5x <101312486+naveen5x@users.noreply.github.com>
2022-11-18 22:52:07 +0800

    Update us_accounts_sf.py

--- us_accounts_sf.py
+++ us_accounts_sf.py
@@ -71,6 +71,7 @@ state = st.multiselect(
     'Choose the states that you want to filter'
     ,sorted(set(filtered_data['state'].str.lower())))
 
+filtered_data = filtered_data.loc[filtered_data['state'].str.lower().isin(state)]  
 filtered_data = pd.DataFrame(filtered_data, columns=['account_name','city','state','zip','lat','lon','active_acv'])  
 '''
 if filtered_data.empty:
